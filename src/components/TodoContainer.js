7225bd2d9142f3dfbf96aea59322c52dd55cf94d
safafa <safaaballagh97@gmail.com>
2021-09-21 16:52:14 +0100

    Add router

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-param-reassign */
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import InputTodo from './InputTodo';
 import TodosList from './TodosList';
+import About from '../pages/About';
+import NotMatch from '../pages/NotMatch';
+import Navbar from './Navbar';
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState([]);
@@ -47,18 +51,31 @@ const TodoContainer = () => {
   };
 
   return (
-    <div className="container">
-      <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={addTodoItem} />
-        <TodosList
-          todos={todos}
-          handleChangeProps={handleChange}
-          deleteTodoProps={delTodo}
-          setUpdate={setUpdate}
-        />
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/">
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={addTodoItem} />
+              <TodosList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+              />
+            </div>
+          </div>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
 import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>, document.getElementById('root'),
 );
